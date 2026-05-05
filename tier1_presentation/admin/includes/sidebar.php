0c9a84d5c2ebe22567f057cf0d523eb8ffb123a3
Chathura Ravishan <chathuraravishan73@gmail.com>
2026-05-05 23:45:42 +0530

    Add Trainers link to sidebar navigation

--- tier1_presentation/admin/includes/sidebar.php
+++ tier1_presentation/admin/includes/sidebar.php
@@ -55,6 +55,17 @@ function nav_class(string $page, string $active): string {
             View Members
         </a>
 
+        <a href="trainers.php" class="<?php echo nav_class('trainers', $active_page); ?>">
+            <span class="nav-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M6 4v16M18 4v16"/>
+                    <path d="M4 8h4M16 8h4M4 16h4M16 16h4"/>
+                    <line x1="8" y1="12" x2="16" y2="12"/>
+                </svg>
+            </span>
+            Trainers
+        </a>
+
         <a href="payments.php" class="<?php echo nav_class('payments', $active_page); ?>">
             <span class="nav-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -77,7 +88,7 @@ function nav_class(string $page, string $active): string {
         </a>
 
     </nav>
-
+    
     <div class="sidebar-footer">
         &copy; <?php echo date('Y'); ?> Fitness Hub
     </div>
